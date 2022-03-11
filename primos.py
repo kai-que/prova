d81ebfd3d6c2bc9c3a852bcb4ee9019370ebf1b2
kai-que <98480414+kai-que@users.noreply.github.com>
2022-03-10 21:51:36 -0300

    Update primos.py

--- primos.py
+++ primos.py
@@ -11,16 +11,13 @@ O exercício possui algumas dicas de implementação na forma de comentários.
 # Acrescentamos os novos primos a lista de primos.
 # Assumimos que o numero e primo e tentamos encontrar o divisor.
 # Um numero nao pode ser primo se for divisivel por outro primo.
-e_primo = False
-e_primo = True
-for p in primos:
-    for x in nums:
-        if e_primo:
-            if x % p == 0:
-                n = int(input("n: "))
-                nums = range(2, n + 1)
-                nums = range(2, n)
-                nums = range(n)
-                primos = [2, 3]
-                primos.append(x)
-                print(primos)
+import math
+
+
+N = int(input("N: "))
+A = list(range(2, N))
+for i in range(2, int(math.sqrt(N)+1)):
+  if i in A:
+    for j in range(i**2, N, i):
+      if j in A: A.remove(j)
+print(A)
