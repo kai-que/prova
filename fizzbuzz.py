fab99553e1e9ebd65eb33789c8fe3a459530c9dd
kai-que <98480414+kai-que@users.noreply.github.com>
2022-03-10 21:46:51 -0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,18 +9,15 @@ de 3 por "fizz", os múltiplos de 5 por "buzz" e os múltiplos de ambos por "fiz
 * if: 1
 """
 
-break
-elif i % 15 == 0:
-    elif i % 3 == 0:
-        elif i % 5 == 0:
-            else:
-                for i in range(1, n + 1):
-                    for i in range(n + 1):
-                        for i in range(n):
-                            if n <= 0:
-                                n = int(input(("n: ")))
-                                print("buzz")
-                                print("fizz")
-                                print("fizzbuzz")
-                                print("numero invalido")
-                                print(i)
+n = int(input(("n: ")))
+for i in range(n):
+    if n <= 0:
+        print(i)
+else:
+        print("número inválido")
+
+for i in range(n + 1):
+        print("fizzbuzz")
+   
+for i in range(1, n + 1):
+        print("fizz")
