f8ef93bfad2cf49e7fd859170e010a594ad06b1f
kai-que <98480414+kai-que@users.noreply.github.com>
2022-03-10 21:50:42 -0300

    Update pares.py

--- pares.py
+++ pares.py
@@ -8,16 +8,15 @@ elementos, inclua a string vazia como segundo elemento do par final
 * lst: 1
 """
 
-else:
-    for i in range(0, n, 2):
+pares = []
+strings = input("lst: ").split(",")
+n = len(pares)
+n = len(strings)
+for i in range(0, n, 2):
+    if i == n - 1:
+        print(x, y)
+    else:
         for x, y in pares:
-            if i == n - 1:
-                n = len(pares)
-                n = len(strings)
-                pares = []
-                pares.append([x, y])
-                print(x, y)
-                strings = input("lst: ").split(",")
-                x = strings[i]
-                y = ""
-                y = strings[i + 1]
+            pares.append([x, y])
+            x = strings[i]
+            y = strings[i + 1]
